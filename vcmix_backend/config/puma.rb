e6cc6cb0f93939c7288e28a466f356f9e830e22b
sunchi <sunchi1119@gmail.com>
2017-11-13 09:51:55 +0800

    set production env

--- vcmix_backend/config/puma.rb
+++ vcmix_backend/config/puma.rb
@@ -4,6 +4,7 @@ state_path "/var/www/pids/vcmix_backend_puma.state"
 bind 'unix:/var/www/pids/vcmix_backend.sock'
 daemonize false
 port 7001
+environment 'production'
 workers (ENV["workers"] || 4)
 threads (ENV["min_threads"] || 8), (ENV["max_threads"] || 8)
 preload_app!
